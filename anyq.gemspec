fd87521b930f83079277c43e597251f0a9230dc3
Luis Angel Ortega <angel-ortega12@hotmail.com>
2022-12-10 15:27:39 -0600

    :sparkles: Given the project a gem structure

---
+++ anyq.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'anyq'
+  s.version     = '0.0.1'
+  s.summary     = 'Give me any quote'
+  s.description = 'A gem that prints a random quote form a collection'
+  s.authors     = ['Luis Angel Ortega']
+  s.email       = 'hey@luisangel.me'
+  s.files       = ['lib/anyq.rb']
+  s.homepage    = 'https://github.com/LinkSake/anyq'
+  s.license     = 'GPL-3.0-or-later'
+end
--- lib/anyq.rb
+++ lib/anyq.rb
@@ -4,31 +4,31 @@ require 'optparse'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: anyq [options]"
-  opts.program_name = "anyq"
-  opts.version = "v0.1"
+  opts.banner = 'Usage: anyq [options]'
+  opts.program_name = 'anyq'
+  opts.version = '0.0.1'
 
-  opts.on("-q", "--quote", "Print only the quote") do |q|
+  opts.on('-q', '--quote', 'Print only the quote') do |q|
     options[:quote] = q
   end
 
-  opts.on("-a", "--author", "Print the author of the quote") do |a|
+  opts.on('-a', '--author', 'Print the author of the quote') do |a|
     options[:author] = a
   end
 
-  opts.on("-s", "--source", "Print the source of the quote") do |s|
+  opts.on('-s', '--source', 'Print the source of the quote') do |s|
     options[:source] = s
   end
 
-  opts.on("-Q", "--new-quote QUOTE", "Add a quote to the JSON file") do |q|
+  opts.on('-Q', '--new-quote QUOTE', 'Add a quote to the JSON file') do |q|
     options[:new_quote] = q
   end
 
-  opts.on("-A", "--new-author AUTHOR", "Specify the author of the quote") do |a|
+  opts.on('-A', '--new-author AUTHOR', 'Specify the author of the quote') do |a|
     options[:new_author] = a
   end
 
-  opts.on("-S", "--new-source SOURCE", "Specify the source of the quote") do |s|
+  opts.on('-S', '--new-source SOURCE', 'Specify the source of the quote') do |s|
     options[:new_source] = s
   end
 end.parse!
@@ -38,15 +38,15 @@ quotes = JSON.parse(quotes_file)
 
 if options[:new_quote]
   new_quote = {
-    "quote": options[:new_quote],
-    "author": options[:new_author] || "Unknown",
-    "source": options[:new_source] || "Unknown"
+    'quote': options[:new_quote],
+    'author': options[:new_author] || 'Unknown',
+    'source': options[:new_source] || 'Unknown'
   }
   quotes << new_quote
 
   File.write('./quotes.json', JSON.generate(quotes))
 
-  puts "New quote added"
+  puts 'New quote added'
 else
   random_quote = quotes[SecureRandom.random_number(quotes.length)]
 
